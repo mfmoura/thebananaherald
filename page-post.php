8aae819598da4aff8adefe844d5f8dde654a17f3
Felipe Anjos <contato@felipeanjos.com.br>
2015-11-05 02:48:00 -0200

    Pagina de post

---
+++ page-post.php
@@ -0,0 +1,33 @@
+<?php include "header.php" ?>
+<main id="post">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="post-area">
+          <h2 class="post-title"> Titulo do post</h2>
+          <img class="post-img" src="images/post.png" />
+          <p class="post-txt">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+
+        <div class="post-area">
+          <h2 class="post-title"> Titulo do post</h2>
+          <img class="post-img" src="images/post.png" />
+          <p class="post-txt">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+
+        <div class="post-area">
+          <h2 class="post-title"> Titulo do post</h2>
+          <img class="post-img" src="images/post.png" />
+          <p class="post-txt">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php include "footer.php" ?>
